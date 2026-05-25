f6229cf092c9b3a3e753fa98192311c0eb894a95
Bryan Heinz <bryannheinz@gmail.com>
2026-05-26 00:06:16 +0700

    Fix: Bypass tombol" Login, Register, Logout

--- src/components/DashboardNavbar.jsx
+++ src/components/DashboardNavbar.jsx
@@ -4,8 +4,8 @@ export default () => {
   return (
     <nav class="bg-white border-b border-gray-200 shadow-sm">
       <div class="max-w-6xl mx-auto px-10 py-4 flex items-center justify-between">
-        
-        
+
+
         <div class="flex items-center gap-3">
           <div class="w-10 h-10 rounded-xl bg-[#1baa6a] flex items-center justify-center shadow">
             <span class="text-white font-bold">
@@ -21,7 +21,7 @@ export default () => {
           </A>
         </div>
 
-        
+
         <div class="hidden md:flex items-center gap-8">
           <A
             href="/dashboard"
@@ -38,20 +38,23 @@ export default () => {
           </A>
         </div>
 
-        
-        <div class="flex items-center gap-4">
-          
-          
-        <A
-        href="/profile"
-        class="text-[#1a1a2e] font-medium"
-        >
-        Profile
-        </A>
 
-          <button class="bg-red-500  text-white px-4 py-2 rounded-xl font-medium transition-colors">
-            Keluar
-          </button>
+        <div class="flex items-center gap-4">
+
+
+          <A
+            href="/profile"
+            class="text-[#1a1a2e] font-medium"
+          >
+            Profile
+          </A>
+
+          {/* Bypass logout sementara  */}
+          <A href="/">
+            <button class="bg-red-500  text-white px-4 py-2 rounded-xl font-medium transition-colors">
+              Keluar
+            </button>
+          </A>
         </div>
       </div>
     </nav>
